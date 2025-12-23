d5e6c6bdefc0c4430ba4fec446af8d45e273a058
alnasrstarsecurity-nss <alnasrstarit@gmail.com>
2025-12-23 10:53:00 +0300

    Update aqcl.js

--- js/aqcl.js
+++ js/aqcl.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
 /* ===============================
    CONFIG
 ================================ */
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxZRdLgjv651p2BbSNETRwAnkkilntvfiNor4vzJS034_G3M7_Nf0oMVnE77TfliUcN-A/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxK4ei0xe7kGPSgE8GSupveuMS8z0B-SbfN7ipmvtgr1Euh_QfstQxAu5qeQQ5WxIqBtw/exec";
 
 const form = document.getElementById("aqclForm");
 const status = document.getElementById("status");
