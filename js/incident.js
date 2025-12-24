e8816bbd95e1e0a9a59542fe120bf12f77c58100
alnasrstarsecurity-nss <alnasrstarit@gmail.com>
2025-12-24 15:41:41 +0300

    Update incident.js

--- js/incident.js
+++ js/incident.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
 ================================ */
 const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxELlSWsaxSsJSyrJRg4zw/exec";
 
-const form = document.getElementById("aqclForm");
+const form = document.getElementById("incident");
 const status = document.getElementById("status");
 
 /* ===============================
