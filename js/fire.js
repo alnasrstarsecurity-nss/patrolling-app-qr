e7173a33838b520cbc021164149ca74503bc9f65
alnasrstarsecurity-nss <alnasrstarit@gmail.com>
2026-01-08 15:07:59 +0300

    Update fire.js

--- js/fire.js
+++ js/fire.js
@@ -1,4 +1,4 @@
-const FIRE_SCRIPT_URL ="https://script.google.com/macros/s/AKfycbwgbt1hO1Hc8ULb42SbQKL-aO7yM0XFeDpPltiuWjEahm6RdLQZzxREkmv8Qx1Ht5hDGg/exec";
+const FIRE_SCRIPT_URL ="https://script.google.com/macros/s/AKfycbyNY9LEnlk8aBUo-JP_p4avKuuzC3qBMk-Ra0lLZWiryY-eqbcJSh9-FNlgS_G8J5Jr/exec";
 
 const form = document.getElementById("fireForm");
 const status = document.getElementById("status");
