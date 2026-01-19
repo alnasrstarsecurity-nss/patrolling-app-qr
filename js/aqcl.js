a833db89f1bd2c1bcdfe5b877d90bb83c12f5847
alnasrstarsecurity-nss <alnasrstarit@gmail.com>
2026-01-19 10:45:48 +0300

    Update aqcl.js

--- js/aqcl.js
+++ js/aqcl.js
@@ -266,10 +266,6 @@ form.addEventListener("submit", e => {
     guard1CAG: guard1CAG,
     guard2CAG: guard2CAG,
 
-    attachmentType: Array.from(
-    form.querySelectorAll('input[name="attachmentType"]:checked')
-  ).map(c => c.value).join(", "),
-
   attach1: await filesToBase64(form.attach1, 10),
   attach2: await fileToBase64(form.attach2),
   attach3: await fileToBase64(form.attach3),
